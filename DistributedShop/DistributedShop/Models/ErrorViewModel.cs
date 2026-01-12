df5bd17956aafcfcb723205c0861632c16a92b3f
SidorencuOanaAlexandra <osidorencu@yahoo.com>
2026-01-12 14:16:51 +0200

    .

---
+++ DistributedShop/DistributedShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DistributedShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
